e5de14ba010491e8a63484b57cee68edc0dbc937
Jan <janx.fetalvero@gmail.com>
2022-08-22 22:22:42 +0800

    calculator phase 1.2

--- app.js
+++ app.js
@@ -8,52 +8,69 @@ const num7 = document.querySelector('#num7');
 const num8 = document.querySelector('#num8');
 const num9 = document.querySelector('#num9');
 const num0 = document.querySelector('#num0');
-const equals = document.querySelector('#equal');
+const equals = document.getElementById('#equal');
 const allBtns = document.querySelectorAll('.numBtn');
 const plus = document.querySelector('#plus');
 const minus = document.querySelector('#minus');
 const multi = document.querySelector('#multi');
 const division = document.querySelector('#divide');
-const displayValue = document.querySelector('.display');
+const display2 = document.querySelector('.display2');
 const operator = document.querySelectorAll('.operator');
+let display1 = document.querySelector('.display1');
+let currentDisplay1 = '';
+let prevDisplay2 = '';
+let haveDot = false;
+let previousOperation = '';
+let total = null;
 
-function display() {
-    allBtns.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            displayValue.innerText += e.target.innerText;
-        })
+allBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+
+        if (e.target.innerText === "." && haveDot) {
+            return;
+        } else if (e.target.innerText === "." && haveDot == false) {
+            haveDot = true
+        }
+        currentDisplay1 += e.target.innerText;
+        display2.innerText = currentDisplay1;
     })
+})
+
+operator.forEach(op => {
+    op.addEventListener('click', (e) => {
+
+
+        let operationName = e.target.innerText;
+        if (display1 && display2) {
+            operations();
+        } else {
+            total = parseFloat(display2)
+        }
+
+        clearVar(operationName);
+        previousOperation = operationName;
+        console.log(total)
+
+
+    })
+
+})
+
+const clearVar = (operationV) => {
+    prevDisplay2 += currentDisplay1 + ' ' + operationV;
+    display1.innerText = prevDisplay2;
+    display2.innerText = '';
+    currentDisplay1 = '';
+    console.log(total)
 }
-display()
+
 
 //to do tomorrow
-// once the operator is clicked the value will go to the prev-display
-// then make the calculation work
+// make the the Nan result work
 
 
-const add = (ad1, ad2) => {
-
-    return ad1 + ad2;
-}
-const substract = (sub1, sub2) => {
-
-    return sub1 - sub2;
-}
-const multiply = (mul1, mul2) => {
-
-    return mul1 * mul2;
-}
-const divide = (div1, div2) => {
-
-    return div1 / div2;
-}
-const operate = (a, b) => {
-
-    // if textContent is + then add();
-    //if textContent is - then substract();
-    //if textContent is * then multiply();
-    //if text content is / then divide();
-
-
-
+const operations = () => {
+    if (previousOperation === '+') {
+        total = parseFloat(total) + parseFloat(display2);
+    }
 }
