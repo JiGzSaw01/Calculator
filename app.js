83a76b0f85c3d81a885910891486ff90f5022a59
Jan <janx.fetalvero@gmail.com>
2022-09-05 21:11:38 +0800

    calculator final phase

--- app.js
+++ app.js
@@ -90,3 +90,34 @@ clearPrev.addEventListener('click', (e) => {
     lowerDisplay = '';
     displayLower.innerText = '';
 });
+
+document.addEventListener('keydown', (e) => {
+    if (
+        e.key === '0' || e.key === '0' ||
+        e.key === '1' || e.key === '2' ||
+        e.key === '3' || e.key === '4' ||
+        e.key === '5' || e.key === '6' ||
+        e.key === '7' || e.key === '8' ||
+        e.key === '9' || e.key === '.') {
+        buttonClicked(e.key);
+    } else if (e.key === '+' || e.key === '-' || e.key === '/' || e.key === '*') {
+        clickedOperator(e.key);
+    } else if (e.key === 'Enter' || e.key === '=') {
+        equals.click();
+    };
+});
+
+const buttonClicked = (key) => {
+    allBtns.forEach((button) => {
+        if (button.innerText === key) {
+            button.click();
+        };
+    });
+};
+const clickedOperator = (key) => {
+    operator.forEach((button) => {
+        if (button.innerText === key) {
+            button.click();
+        };
+    });
+};
